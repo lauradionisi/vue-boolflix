32efd497636378320ee6ccf21c103c56822c80a2
Laura <laura.dionisi91@gmail.com>
2021-04-08 20:40:58 +0200

    Boolflix milestone 4 non completa

--- js/main.js
+++ js/main.js
@@ -65,7 +65,7 @@ var app = new Vue({
         },
 
         getPoster(poster) {
-            return `https://image.tmdb.org/t/p/w185/${poster}`;
+            return `https://image.tmdb.org/t/p/w342/${poster}`;
         }
 
        
